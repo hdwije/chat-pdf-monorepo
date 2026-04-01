b810817a6f61c07b48a1d591a8176ac9a38422b2
hdwije <hwijegunarathna@gmail.com>
2026-04-02 00:31:04 +0530

    feat: integrate Turbo for improved build performance and update API to return user profile

--- apps/api/src/app.service.ts
+++ apps/api/src/app.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { UserProfile } from '@repo/types';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getProfile(): UserProfile {
+    return {
+      id: 'abc123',
+      name: 'Hemantha',
+      email: 'hemantha@example.com',
+    };
   }
 }
