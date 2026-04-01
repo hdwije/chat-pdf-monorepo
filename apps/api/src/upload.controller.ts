34d33f0418c1c26bfdc605d651fb1d15ebe60602
hdwije <hwijegunarathna@gmail.com>
2026-04-02 01:12:21 +0530

    feat: add @types/multer package and update tsconfig to include multer types

---
+++ apps/api/src/upload.controller.ts
@@ -0,0 +1,37 @@
+import {
+  Controller,
+  FileTypeValidator,
+  MaxFileSizeValidator,
+  ParseFilePipe,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+
+@Controller('upload')
+export class UploadController {
+  @Post('pdf')
+  @UseInterceptors(FileInterceptor('file'))
+  uploadFile(
+    @UploadedFile(
+      new ParseFilePipe({
+        validators: [
+          new MaxFileSizeValidator({ maxSize: 10 * 1024 * 1024 }),
+          new FileTypeValidator({ fileType: 'application/pdf' }),
+        ],
+      }),
+    )
+    file: Express.Multer.File,
+  ) {
+    console.log('Recieved file', file);
+
+    const uploadedFile = file as { originalname: string; size: number };
+
+    return {
+      message: 'File uploaded successfully',
+      filename: uploadedFile.originalname,
+      size: uploadedFile.size,
+    };
+  }
+}
